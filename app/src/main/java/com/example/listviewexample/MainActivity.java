0227a2f71cda291a42fc0b47ea132e89f5c26584
estebanparrarodriguez <esteban.parrarodriguez@belmont.edu>
2023-06-03 10:24:21 -0500

    Added boilerplate starter code

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -10,5 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int selection = 1;
+        populateListView(selection);
+    }
+
+    private void populateListView(int selection) {
+        switch (selection) {
+            case 1:
+                useStringResource();
+                break;
+            case 2:
+                usingDataArray();
+                break;
+
+        }
+    }
+
+    /**
+     * This method populates the list view using a string resource containing
+     * the content of a strings array resource
+     */
+    private void useStringResource() {
+
+    }
+
+    /**
+     * This method populates the list view using a string resource containing the list of a data array
+     */
+    private void usingDataArray() {
+
+
     }
 }
