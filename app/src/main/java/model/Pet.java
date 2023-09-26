4dac76720550f73054e06c527e391f21fc71ce44
tony-tomass <tonynguyen984@gmail.com>
2023-09-26 13:02:34 -0500

    Added stuff

---
+++ app/src/main/java/model/Pet.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Pet {
+
+    String name, breed;
+    int age;
+
+    public Pet(String thisName, String thisBreed, int thisAge) {
+        name = thisName;
+        breed = thisBreed;
+        age = thisAge;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
